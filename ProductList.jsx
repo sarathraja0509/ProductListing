041f6f0ebaff2c75794cba263417a1763f8b1098
sarathraja0509 <57962973+sarathraja0509@users.noreply.github.com>
2023-11-14 11:36:00 +0530

    Update ProductList.jsx

--- ProductList.jsx
+++ ProductList.jsx
@@ -1,17 +1,14 @@
 import React, { useState, useEffect } from 'react';
+import { TextField, Button, Select, MenuItem, Grid } from '@material-ui/core';
 
 const ProductList = () => {
-    // State variables
-    const [products, setProducts] = useState([]);
     const [searchKeyword, setSearchKeyword] = useState('');
     const [categories, setCategories] = useState([]);
     const [selectedCategory, setSelectedCategory] = useState('');
     const [filteredProducts, setFilteredProducts] = useState([]);
 
     useEffect(() => {
-        // Fetch products on component mount
         getProducts();
-        // Fetch categories on component mount
         getProductCategories();
     }, []);
 
@@ -19,7 +16,6 @@ const ProductList = () => {
         try {
             const response = await fetch('https://dummyjson.com/products');
             const data = await response.json();
-            setProducts(data);
             setFilteredProducts(data.products); // Initialize filteredProducts with all products
         } catch (error) {
             console.error('Error fetching products:', error);
@@ -59,31 +55,41 @@ const ProductList = () => {
 
     return (
         <div>
-            {/* Category Dropdown */}
-            <select onChange={(e) => handleCategoryChange(e.target.value)}>
-                <option value="" selected disabled>
-                    Select Category
-                </option>
-                {categories.map((category) => (
-                    <option key={category} value={category}>
-                        {category}
-                    </option>
-                ))}
-            </select>
+            <Grid container spacing={2}>
+
+                {/* Category Dropdown */}
+                <Grid item xs={12} sm={4}>
+                    <Select
+                        displayEmpty
+                        value={selectedCategory}
+                        onChange={(e) => handleCategoryChange(e.target.value)}
+                        fullWidth
+                    >
+                        <MenuItem value="" disabled>
+                            Select Category
+                        </MenuItem>
+                        {categories.map((category) => (
+                            <MenuItem key={category} value={category}>
+                                {category}
+                            </MenuItem>
+                        ))}
+                    </Select>
+                </Grid>
+            </Grid>
 
             {/* Display Products with Thumbnails */}
-            <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+            <Grid container spacing={2}>
                 {filteredProducts.map((product) => (
-                    <div key={product.id} style={{ width: '25%', padding: '10px' }}>
+                    <Grid key={product.id} item xs={6} sm={3}>
                         <img
                             src={product.thumbnail}
                             alt={product.title}
                             style={{ width: '100%', height: 'auto' }}
                         />
                         <p>{product.title}</p>
-                    </div>
+                    </Grid>
                 ))}
-            </div>
+            </Grid>
         </div>
     );
 };
